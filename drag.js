8bf5aeaf97fd62ad1bf247f40548f084f7445a4b
jersey <lizexi711@gmail.com>
2015-07-23 13:37:11 +0800

    new

--- drag.js
+++ drag.js
@@ -1,12 +1,50 @@
 /**
  * Created by jersey on 2015/7/22.
  */
+
+// 构造函数Drag
 function Drag(id){
-    var obj=document.getElementById(id);
-    this.disX=0;
-    this.disY=0;
+    this.obj = document.getElementById(id);
+    this.disX = 0;
+    this.disY = 0;
 }
 
-Drag.prototyp.init = function(){
+Drag.prototype.init = function (){
+    // this指针
+    var me = this;
 
+    this.obj.onmousedown = function (e){
+        var e = e || event;
+        me.mouseDown(e);
+
+        // 阻止默认事件
+        return false;
+    };
+};
+
+Drag.prototype.mouseDown = function (e){
+    // this指针
+    var me = this;
+    this.disX = e.clientX - this.obj.offsetLeft;
+    this.disY = e.clientY - this.obj.offsetTop;
+
+    document.onmousemove = function (e){
+        var e = e || window.event;
+
+        me.mouseMove(e);
+    };
+
+    document.onmouseup = function (){
+        me.mouseUp();
+    }
+};
+
+Drag.prototype.mouseMove = function (e){
+    this.obj.style.left = (e.clientX - this.disX) + 'px';
+    this.obj.style.top = (e.clientY - this.disY) + 'px';
+};
+
+Drag.prototype.mouseUp = function (){
+    document.onmousemove = null;
+    document.onmouseup = null;
 };
