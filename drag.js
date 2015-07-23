a3ac85f668d4cba0f7d576e2a03068e46584f2de
jersey <lizexi711@gmail.com>
2015-07-23 10:31:46 +0800

    new

--- drag.js
+++ drag.js
@@ -1,3 +1,12 @@
 /**
  * Created by jersey on 2015/7/22.
  */
+function Drag(id){
+    var obj=document.getElementById(id);
+    this.disX=0;
+    this.disY=0;
+}
+
+Drag.prototyp.init = function(){
+
+};
